91fa5f94b301e44a8e26ce968d14d791edfd7086
Silvain Raveau <silvain@raveau.net>
2025-05-08 15:48:12 +0200

    refactor: rename ApiController to HelloController

--- src/main/java/com/starter/controller/HelloController.java
+++ src/main/java/com/starter/controller/HelloController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
-public class ApiController {
+public class HelloController {
 
     @GetMapping("/hello")
     public String hello() {
